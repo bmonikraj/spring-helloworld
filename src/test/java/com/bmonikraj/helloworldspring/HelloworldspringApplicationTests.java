052955b6d4537a19db34d2a4f1f036152f42391f
MONIK RAJ BEHERA <bmonikraj@gmail.com>
2021-01-27 01:52:53 +0530

    adding initial files

---
+++ src/test/java/com/bmonikraj/helloworldspring/HelloworldspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmonikraj.helloworldspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
